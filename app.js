e626a9739bd02e5fbecfad2fbebdb57ebe68565b
allenres <allen.resulidze@gmail.com>
2025-10-10 19:45:13 -0700

    converted to a node application

---
+++ app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+// Enable static file serving (client side file that does not communicate with database)
+app.use(express.static('public'));
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+})
